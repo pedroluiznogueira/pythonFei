5c8d28df4236d70076679fe59f815325f5d129f1
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-08 13:34:47 -0300

    For loop

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
-# while loop =  a statement that will execute it's block of code,
-#               as long as it's condition remains true
+# for loop =    a statement that will execute it's block of code
+#                      a limited amount of times
+#
+#               while loop = unlimited
+#               for loop = limited
 
-name = "" # that is the of making the variable name to exist, is kind of like the do while loop
+import time
 
-while len(name) == 0: # while the input is nothing
-    name = input("Enter your name: ")
+for i in range(10):
+     print(i + 1)
 
-print("Hello "+name)
+# for i in range(50,101,2):
+#     print(i)
+#
+# for i in "Bro Code":
+#     print(i, end="")
+
+# for seconds in range(10,0,-1):
+#     print(seconds)
+#     time.sleep(2) # goes inside of the seconds variable and use the funntion time
+#     print("Happy New Year!")
