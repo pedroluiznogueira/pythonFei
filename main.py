e950fbda695b06a17af66962e348299ecb7f40fc
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-08 13:19:29 -0300

    Funções para manipulação de strings

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
-# multiple assignment = allows us to assign multiple variables at the same time in one line of code
 
 name = "Bro"
-age = 21
-attractive = True
 
-name, age, attractive = "Bro", 21, True
-
-print(name, end=" ")
-print(age, end=""+" ")
-print(attractive, end=""+" ")
-
-# Spongebob = 30
-# Patrick = 30
-# Sandy = 30
-# Squidward = 30
-
-# Spongebob = Patrick = Sandy = Squidward = 30
-
-# print(Spongebob)
-# print(Patrick)
-# print(Sandy)
-# print(Squidward)
+print(len(name)) #technically this is a function
+print(name.find("o")) #return the index of "o" in the string
+print(name[name.find("o")])
+print(name.capitalize()) #captaliza an specific letter of the string
+print(name.upper()) #captalize the whole string
+print(name.lower()) #makes the opposite
+print(name.isdigit()) #string as input
+print(name.isalpha()) #string as input
+print(name.count("o")) #how many specif chars are in the string
+print(name.replace("o","a")) #change a char
+print(name*3) #multiply the string by 3
