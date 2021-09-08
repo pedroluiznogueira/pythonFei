af5933cec421a2d4643fe70a06fce8a44fee31d7
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-08 13:23:07 -0300

    Pedindo input do usuário

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
 
-name = "Bro"
-
-print(len(name)) #technically this is a function
-print(name.find("o")) #return the index of "o" in the string
-print(name[name.find("o")])
-print(name.capitalize()) #captaliza an specific letter of the string
-print(name.upper()) #captalize the whole string
-print(name.lower()) #makes the opposite
-print(name.isdigit()) #string as input
-print(name.isalpha()) #string as input
-print(name.count("o")) #how many specif chars are in the string
-print(name.replace("o","a")) #change a char
-print(name*3) #multiply the string by 3
+print("Hello "+name)
+print("You are "+str(age)+" years old")
+print("You are "+str(height)+"cm tall")
