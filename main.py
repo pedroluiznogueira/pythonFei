0909514cc3b054a4b1538f0b93c6238f0c69b119
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-08 13:30:49 -0300

    While loop

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
+# while loop =  a statement that will execute it's block of code,
+#               as long as it's condition remains true
 
-# slicing = create a substring by extracting elements from another string
-#           indexing[] or slice()
-#           [start:stop:step]
+name = "" # that is the of making the variable name to exist, is kind of like the do while loop
 
-name = "Bro Code"
+while len(name) == 0: # while the input is nothing
+    name = input("Enter your name: ")
 
-first_name = name[:3]       # [0:3]
-last_name = name[3:]        # [4:end]
-funky_name = name[::2]      # [0:end:2]
-reversed_name = name[::-1]  # [0:end:-1]
-
-print(first_name)
-print(last_name)
-print(funky_name)
-print(reversed_name)
-
-website1 = "http://google.com"
-website2 = "http://wikipedia.com"
-
-slice = slice(7,-4) # walking backwards in the string
-
-print(website1[slice]) # call the function slice as a index to the string
-print(website2[slice]) # same
+print("Hello "+name)
