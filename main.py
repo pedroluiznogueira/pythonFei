b9ac1e7223aaf94b6a940dc541dff3f5fe365e18
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-08 13:25:22 -0300

    Funções da biblioteca math

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-name = input("What is your name?: ")
-age = int(input("How old are you?: "))
-height = float(input("How tall are you?: "))
+import math
 
-print("Hello "+name)
-print("You are "+str(age)+" years old")
-print("You are "+str(height)+"cm tall")
+pi = -3.14
+x = 1
+y = 2
+z = 3
+
+print(round(pi)) #next to floor
+print(math.ceil(pi)) #next whoe integer
+print(math.floor(pi)) #round floor number
+print(abs(pi)) #absolute number
+print(pow(pi,2)) #first argument is the base, and the second is the expoent
+print(math.floor(math.sqrt(9))) #the argument is the number wich square root you want to calculate
+print(max(x,y,z)) #comparision
+print(min(x,y,z)) #comparision
