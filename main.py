90c2ef82d64f8547eaaf0db3a9486deb6a9f430b
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-09-08 12:57:50 -0300

    Tipos de dado

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Commit inicial branch super")
+#variable = a container for a value. Behaves as the value that it contains
+
+#string = a series of characters
+first_name = "Bro"
+last_name = "Code"
+full_name = first_name +" "+ last_name
+print("Hello "+full_name)
+# print(type(first_name))
+
+#int = a whole integer
+age = 21
+age += 1
+print("Your age is: "+str(age))
+# print(type(age))
+
+#float = a decimal number
+height = 250.5
+print("Your height is: "+str(height)+"cm")
+# print(type(height))
+
+#boolean = True or False
+human = True
+print("Are you a human: "+str(human))
+# print(type(human))
