6714f160ebdaa7f1009750178792fc7421a5c2f5
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-10-06 18:16:00 -0300

    Atualizando arquivos

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-# tuple =   collection which is ordered and unchangeable
-#                used to group together related data
-
-student = ("Bro",21,"male") # lists we use "[....]" and in tuples we use "(....)"
-
-print(student.count("Bro")) # go inside of the tuple student and use the function count
-                            # wich will count the number of "Bro"(s) in the tuple
-print(student.index("male")) # go inside of the tuple studente and use the function index
-                             # wich will return the index of "male" in the tuple
-
-for x in student:
-    print(x)
-
-if "Bro" in student:
-    print("Bro is here!")
+valor = int( input("Digite o valor:") )
+esp = 0
+while valor > 0 :
+    d=valor%10
+    esp=esp*10+d
+    valor=valor//10
+print("Resultado:", esp)
